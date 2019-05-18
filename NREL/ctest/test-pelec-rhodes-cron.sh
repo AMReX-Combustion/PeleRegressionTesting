b035d91eb4772c218922843ca563768abd09b6b3
Jon Rood <jon.rood@nrel.gov>
2019-05-17 21:48:01 -0600

    Update crontab for rhodes.

--- NREL/ctest/test-pelec-rhodes-cron.sh
+++ NREL/ctest/test-pelec-rhodes-cron.sh
@@ -6,5 +6,5 @@ set -e
 
 cd /projects/ecp/combustion/pelec-testing-2/logs && \
 nice -n19 ionice -c3 \
-/projects/ecp/combustion/pelec-testing-2/pelec/Testing/test-pelec.sh &> \
+/projects/ecp/combustion/pelec-testing-2/PeleRegressionTesting/NREL/ctest/test-pelec.sh &> \
 "test-pelec-$(date +%Y-%m-%d).log"
