f90e5c562419f525f0feff44ca4593cc9db70a70
Jon Rood <jon.rood@nrel.gov>
2019-09-19 10:56:26 -0600

    Turning verification tests off for address sanitizer.

--- NREL/ctest/test-pelec.sh
+++ NREL/ctest/test-pelec.sh
@@ -227,7 +227,7 @@ test_configuration() {
     cmd "export LSAN_OPTIONS=suppressions=${PELEC_DIR}/build/asan.supp"
     # Can't run ASAN with optimization
     CMAKE_BUILD_TYPE=Debug
-    #VERIFICATION=OFF
+    VERIFICATION=OFF
     #CMAKE_CONFIGURE_ARGS="-DCMAKE_CXX_FLAGS:STRING=-fsanitize=address\ -fno-omit-frame-pointer ${CMAKE_CONFIGURE_ARGS}"
     #CMAKE_CONFIGURE_ARGS="-DCMAKE_LINKER=clang++ -DCMAKE_CXX_LINK_EXECUTABLE=clang++ -DCMAKE_CXX_FLAGS:STRING=\'-fsanitize=address -fno-omit-frame-pointer\' -DCMAKE_EXE_LINKER_FLAGS:STRING=-fsanitize=address ${CMAKE_CONFIGURE_ARGS}"
     #printf "Disabling OpenMP in PeleC for address sanitizer...\n"
