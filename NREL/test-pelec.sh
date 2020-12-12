ed1062b48041c11ec0252f37262a52bddb0545db
Jon Rood <jon.rood@nrel.gov>
2020-12-12 13:40:49 -0700

    Only filter amrex directories in static analysis.

--- NREL/test-pelec.sh
+++ NREL/test-pelec.sh
@@ -161,7 +161,7 @@ test_configuration() {
     cmd "rm ${LOGS_DIR}/pelec-static-analysis.txt || true"
     cmd "cd ${PELEC_DIR}/build && ln -s ${CPPCHECK_ROOT_DIR}/cfg cfg || true"
     cmd "cppcheck --enable=all --inline-suppr --project=compile_commands.json -j 32 -i${PELEC_DIR}/Submodules/AMReX/Src --output-file=${LOGS_DIR}/pelec-static-analysis-temp.txt || true"
-    cmd "awk -v nlines=2 '/Submodules/ {for (i=0; i<nlines; i++) {getline}; next} 1' < ${LOGS_DIR}/pelec-static-analysis-temp.txt > ${LOGS_DIR}/pelec-static-analysis.txt"
+    cmd "awk -v nlines=2 '/Submodules/AMReX/ {for (i=0; i<nlines; i++) {getline}; next} 1' < ${LOGS_DIR}/pelec-static-analysis-temp.txt > ${LOGS_DIR}/pelec-static-analysis.txt"
     WARNINGS1=$(wc -l < ${LOGS_DIR}/pelec-static-analysis.txt | xargs echo -n)
     WARNINGS2=$(bc <<< "$WARNINGS1/3")
     cmd "printf \"%s warnings\n\" \"${WARNINGS2}\" >> ${LOGS_DIR}/pelec-static-analysis.txt"
